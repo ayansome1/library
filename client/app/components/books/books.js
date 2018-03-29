3d85c587eaa1f3fceb6b20b0640ec445c22fce2d
ayan <ayansome1@gmail.com>
2018-03-29 12:47:14 +0530

    modal working

--- client/app/components/books/books.js
+++ client/app/components/books/books.js
@@ -1,14 +1,18 @@
 'use strict';
 /*jshint node:true, quotmark:false*/
 /*global angular,alert,_,moment*/
-angular.module('libApp')
-	.controller('booksController', ['$scope', '$http', 'baseUrl', '$rootScope', '$state', function ($scope, $http, baseUrl, $rootScope, $state) {
-
-
+angular.module('libApp').controller('booksController', [
+	'$scope',
+	'$http',
+	'baseUrl',
+	'$rootScope',
+	'$state',
+	'$mdDialog',
+	function($scope, $http, baseUrl, $rootScope, $state, $mdDialog) {
 		// let  getAllBooks = () => {
 
 		// 	$http.get(baseUrl + "/all-books").then(function (response) {
-				
+
 		// 		console.log(response.data);
 		// 	})
 		// 	.catch(function () {
@@ -19,10 +23,41 @@ angular.module('libApp')
 
 		// getAllBooks();
 		// $scope.abc = "1234243252";
-		$scope.books = "12";
+		$scope.books = '12';
 		$rootScope.tab = 'books';
 
+		$scope.showAdvanced = function(ev) {
+			$mdDialog.show({
+				controller: AddBookController,
+				templateUrl: 'components/books/add-book.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose: true,
+				fullscreen: false, // Only for -xs, -sm breakpoints.
+			});
+			/*.then(
+					function(answer) {
+						$scope.status = 'You said the information was "' + answer + '".';
+					},
+					function() {
+						$scope.status = 'You cancelled the dialog.';
+					}
+				)*/
+		};
 
-		
+		function AddBookController($scope, $mdDialog) {
 
-}]);
+			$scope.x = 546;
+
+			$scope.hide = function() {
+				$mdDialog.hide();
+			};
+
+			$scope.cancel = function() {
+				$mdDialog.cancel();
+			};
+
+	
+		}
+	},
+]);
