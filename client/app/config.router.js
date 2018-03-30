ada92b174ede5960212162b3b1e7c3615effc976
ayan <ayansome1@gmail.com>
2018-03-30 13:15:10 +0530

    prev next book

--- client/app/config.router.js
+++ client/app/config.router.js
@@ -1,28 +1,35 @@
 'use strict';
 /*global angular,noty*/
 
-angular.module('libApp')
+angular
+	.module('libApp')
 
-.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
+	.config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		'$httpProvider',
+		function($stateProvider, $urlRouterProvider, $httpProvider) {
+			$urlRouterProvider.otherwise('/books');
 
-	$urlRouterProvider.otherwise('/books');
+			$stateProvider
+				.state('app', {
+					abstract: true,
+					templateUrl: 'components/common/layout.html',
+				})
 
-	$stateProvider
-		.state('app', {
-			abstract: true,
-			templateUrl: 'components/common/layout.html'
-		})
-		
-		.state('app.books', {
-			url: '/books',
-			templateUrl: 'components/books/books.html',
-		})
+				.state('app.books', {
+					url: '/books',
+					templateUrl: 'components/books/books.html',
+				})
 
-		.state('app.authors', {
-			url: '/authors',
-			templateUrl: 'components/authors/authors.html',
-		});
+				.state('app.books-details', {
+					url: '/books/:id',
+					templateUrl: 'components/books/book-details.html',
+				})
 
-}])
-
-;
+				.state('app.authors', {
+					url: '/authors',
+					templateUrl: 'components/authors/authors.html',
+				});
+		},
+	]);
