ab202021e17a7673f769fc70b3206c39642e86be
ayan <ayansome1@gmail.com>
2018-03-30 18:49:46 +0530

    details pages js

---
+++ client/app/components/authors/author-details.js
@@ -0,0 +1,20 @@
+'use strict';
+/*jshint node:true, quotmark:false*/
+/*global angular,alert,_,moment*/
+angular.module('libApp').controller('authorDetailsController', [
+	'$scope',
+	'$http',
+	'baseUrl',
+	'$rootScope',
+	'$state',
+	'$mdDialog',
+	function($scope, $http, baseUrl, $rootScope, $state,$mdDialog) {
+		
+		$rootScope.tab = '';
+
+
+
+		
+
+	}
+]);
---
+++ client/app/components/books/book-details.js
@@ -0,0 +1,15 @@
+'use strict';
+/*jshint node:true, quotmark:false*/
+/*global angular,alert,_,moment*/
+angular.module('libApp').controller('bookDetailsController', [
+	'$scope',
+	'$http',
+	'baseUrl',
+	'$rootScope',
+	'$state',
+	'$mdDialog',
+	function($scope, $http, baseUrl, $rootScope, $state, $mdDialog) {
+		$rootScope.tab = '';
+
+	}
+]);
